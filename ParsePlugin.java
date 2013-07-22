12e3c34a8ac39e0215c166ca464138decd1d9f95
Avi Vaisenberger <avi@fabulagame.com>
2013-07-22 16:44:45 +0300

    ParsePlugin - CordovaPlugin extension

---
+++ ParsePlugin.java
@@ -0,0 +1,77 @@
+package org.apache.cordova.core.parse;
+
+import java.util.Set;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import com.parse.ParseInstallation;
+import com.parse.PushService;
+
+public class ParsePlugin extends CordovaPlugin {
+	public static final String ACTION_GET_INSTALLATION_ID = "getInstallationId";
+	public static final String ACTION_GET_SUBSCRIPTIONS = "getSubscriptions";
+	public static final String ACTION_SUBSCRIBE = "subscribe";
+	public static final String ACTION_UNSUBSCRIBE = "unsubscribe";
+	
+	@Override
+	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+		if (action.equals(ACTION_GET_INSTALLATION_ID)) {
+			this.getInstallationId(callbackContext);
+			return true;
+		}
+		if (action.equals(ACTION_GET_SUBSCRIPTIONS)) {
+			this.getSubscriptions(callbackContext);
+			return true;
+		}
+		if (action.equals(ACTION_SUBSCRIBE)) {
+			this.subscribe(args.getString(0), callbackContext);
+			return true;
+		}
+		if (action.equals(ACTION_UNSUBSCRIBE)) {
+			this.unsubscribe(args.getString(0), callbackContext);
+			return true;
+		}
+		return false;
+	}
+	
+	private void getInstallationId(final CallbackContext callbackContext) {
+		cordova.getThreadPool().execute(new Runnable() {
+		    public void run() {
+				String installationId = ParseInstallation.getCurrentInstallation().getInstallationId();
+				callbackContext.success(installationId);
+		    }
+		});
+	}
+	
+	private void getSubscriptions(final CallbackContext callbackContext) {
+		cordova.getThreadPool().execute(new Runnable() {
+		    public void run() {
+				 Set<String> subscriptions = PushService.getSubscriptions(MainApplication.getContext());
+				 callbackContext.success(subscriptions.toString());
+		    }
+		});		
+	}
+	
+	private void subscribe(final String channel, final CallbackContext callbackContext) {
+		cordova.getThreadPool().execute(new Runnable() {
+		    public void run() {
+				PushService.subscribe(MainApplication.getContext(), channel, MainActivity.class);
+				callbackContext.success();
+		    }
+		});
+	}
+	
+	private void unsubscribe(final String channel, final CallbackContext callbackContext) {
+		cordova.getThreadPool().execute(new Runnable() {
+		    public void run() {
+				PushService.unsubscribe(MainApplication.getContext(), channel);
+				callbackContext.success();
+		    }
+		});
+	}
+
+}
+
