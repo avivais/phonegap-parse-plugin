a828868abc3b6751b1571fe7a7f98cc5c0302484
Hyliker Cheung <hyliker@themanxgroup.tw>
2013-12-03 11:13:57 +0800

    run getInstallationId, getInstallationObjectId in background thread

--- src/ios/CDVParsePlugin.m
+++ src/ios/CDVParsePlugin.m
@@ -6,20 +6,24 @@
 
 - (void)getInstallationId:(CDVInvokedUrlCommand*) command
 {
-    CDVPluginResult* pluginResult = nil;
-    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
-    NSString *installationId = currentInstallation.installationId;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:installationId]
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    [self.commandDelegate runInBackground:^{
+        CDVPluginResult* pluginResult = nil;
+        PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+        NSString *installationId = currentInstallation.installationId;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:installationId];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
 
 - (void)getInstallationObjectId:(CDVInvokedUrlCommand*) command
 {
-    CDVPluginResult* pluginResult = nil;
-    PFInstallation *currentInstallation = [PFInstallation currentInstallation];
-    NSString *objectId = currentInstallation.objectId;
-    pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:objectId]
-    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    [self.commandDelegate runInBackground:^{
+        CDVPluginResult* pluginResult = nil;
+        PFInstallation *currentInstallation = [PFInstallation currentInstallation];
+        NSString *objectId = currentInstallation.objectId;
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:objectId];
+        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+    }];
 }
 
 @end
